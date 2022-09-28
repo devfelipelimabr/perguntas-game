b95980b5df7d63d9b233a88de7821a3fbb6d8dbd
devfelipelimabr <felipelima85@hotmail.com>
2022-09-28 00:44:21 -0300

    create function for to calc the test grade

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 function score() {
     let sum = 0
 
+    const msg = document.getElementById('score')
+
     const question1 = document.getElementsByName('q1')
     const question2 = document.getElementsByName('q2')
     const question3 = document.getElementsByName('q3')
@@ -13,8 +15,47 @@ function score() {
     const question9 = document.getElementsByName('q9')
     const question10 = document.getElementsByName('q10')
 
-    if (question1[0].checked){
+    if (question1[0].checked) {
         sum++
-        window.alert(`Nota =${sum} - questão correta!`)
-    }
+        msg.innerHTML = (`<p>Questão 1 Correta!</p><br>`)
+    } else { msg.innerHTML = (`<p>Questão 1 Incorreta!</p><br>`) }
+    if (question2[4].checked) {
+        sum++
+        msg.innerHTML += (`<p>Questão 2 Correta!</p><br>`)
+    } else { msg.innerHTML += (`<p>Questão 2 Incorreta!</p><br>`) }
+    if (question3[1].checked) {
+        sum++
+        msg.innerHTML += (`<p>Questão 3 Correta!</p><br>`)
+    } else { msg.innerHTML += (`<p>Questão 3 Incorreta!</p><br>`) }
+    if (question4[0].checked) {
+        sum++
+        msg.innerHTML += (`<p>Questão 4 Correta!</p><br>`)
+    } else { msg.innerHTML += (`<p>Questão 4 Incorreta!</p><br>`) }
+    if (question5[0].checked) {
+        sum++
+        msg.innerHTML += (`<p>Questão 5 Correta!</p><br>`)
+    } else { msg.innerHTML += (`<p>Questão 5 Incorreta!</p><br>`) }
+    if (question6[4].checked) {
+        sum++
+        msg.innerHTML += (`<p>Questão 6 Correta!</p><br>`)
+    } else { msg.innerHTML += (`<p>Questão 6 Incorreta!</p><br>`) }
+    if (question7[1].checked) {
+        sum++
+        msg.innerHTML += (`<p>Questão 7 Correta!</p><br>`)
+    } else { msg.innerHTML += (`<p>Questão 7 Incorreta!</p><br>`) }
+    if (question8[2].checked) {
+        sum++
+        msg.innerHTML += (`<p>Questão 8 Correta!</p><br>`)
+    } else { msg.innerHTML += (`<p>Questão 8 Incorreta!</p><br>`) }
+    if (question9[0].checked) {
+        sum++
+        msg.innerHTML += (`<p>Questão 9 Correta!</p><br>`)
+    } else { msg.innerHTML += (`<p>Questão 9 Incorreta!</p><br>`) }
+    if (question10[1].checked) {
+        sum++
+        msg.innerHTML += (`<p>Questão 10 Correta!</p><br>`)
+    } else { msg.innerHTML += (`<p>Questão 10 Incorreta!</p><br>`) }
+
+    msg.innerHTML += (`<br><p id="finalScore">Sua nota final foi: <span id="scoreStyle">${sum}</span></p>`)
+
 }
